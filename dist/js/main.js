b9e77c15d9e439d7c6f32bd050c90e5fb4c01213
Tomas <stveracek.t@seznam.cz>
2019-07-31 23:32:00 +0200

    carousel section change

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
   $('.loader-bg').hide(2000);
+
+  $('.navbar-toggler-icon').click(function() {
+    $('.nav-toggle').toggle('slow');
+  });
+
+  $('.fa-search').click(function() {
+    $('.search').toggle('slow');
+  });
 });
